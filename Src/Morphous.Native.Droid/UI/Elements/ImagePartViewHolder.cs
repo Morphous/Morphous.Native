574d634142d58890836cc120a4995d3463df4c73
Tom Pratt <tom.pratt@live.com>
2017-03-15 22:29:00 +0100

    navigate to full image

--- Src/Morphous.Native.Droid/UI/Elements/ImagePartViewHolder.cs
+++ Src/Morphous.Native.Droid/UI/Elements/ImagePartViewHolder.cs
@@ -12,13 +12,22 @@ using Android.Widget;
 using Morphous.Native.Models;
 using FFImageLoading.Views;
 using FFImageLoading;
+using GalaSoft.MvvmLight.Messaging;
+using Morphous.Native.Droid.Events;
 
 namespace Morphous.Native.Droid.UI.Elements
 {
     public class ImagePartViewHolder : ElementViewHolder<IImagePart>
     {
-        public ImagePartViewHolder(Context context, LayoutInflater inflater, ViewGroup container, IImagePart element) : base(context, inflater, container, element)
+        private readonly IMessenger _messenger;
+
+        public ImagePartViewHolder(Context context, LayoutInflater inflater, ViewGroup container, IImagePart element) : this(context, inflater, container, element, null)
         {
+        }
+
+        public ImagePartViewHolder(Context context, LayoutInflater inflater, ViewGroup container, IImagePart element, IMessenger messenger) : base(context, inflater, container, element)
+        {
+            _messenger = messenger ?? GalaSoft.MvvmLight.Messaging.Messenger.Default;
         }
 
         protected override void BindView(View view)
@@ -28,9 +37,19 @@ namespace Morphous.Native.Droid.UI.Elements
             var imageView = view.FindViewById<ImageViewAsync>(Resource.Id.imagePart_image);
             imageView.ScaleToFit = true;
 
+            if (Element.Zone.ContentItem.DisplayType == "Summary")
+            {
+                imageView.Click += ImageView_Click;
+            }
+
             ImageService.Instance.LoadUrl($"http://192.168.0.22:96{Element.Url}")
                 .Retry(3, 200)
                 .Into(imageView);
+        }
+
+        private void ImageView_Click(object sender, EventArgs e)
+        {
+            _messenger.Send(new ContentItemSelectedMessage(Element.Zone.ContentItem));
         }
     }
 }
