9dd7d83882e37af77fa238308a2b6f74b216ad23
Tom Pratt <tom.pratt@live.com>
2017-04-02 14:56:52 +0200

    content item cell sort of working but not setting height properly yet

---
+++ Src/Morphous.Native.iOS/UI/ContentItemCell.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Morphous.Native.Models;
+using UIKit;
+
+namespace Morphous.Native.iOS
+{
+    public class ContentItemCell : UITableViewCell
+    {
+        public ContentItemCell(DisplayContext displayContext, IContentItem contentItem, string cellId) : base(UITableViewCellStyle.Default, cellId)
+        {
+            var contentItemView = displayContext.ViewFactory.CreateContentItemView(contentItem);
+            contentItemView.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            ContentView.AddSubview(contentItemView);
+        //    ContentView.AddConstraints(ContentConstraints(contentItemView, this));
+        }
+
+        private NSLayoutConstraint[] ContentConstraints(UIView contentItemView, UIView container)
+        {
+            return new NSLayoutConstraint[] {
+                NSLayoutConstraint.Create (
+                    contentItemView,
+                    NSLayoutAttribute.Left,
+                    NSLayoutRelation.Equal,
+                    container,
+                    NSLayoutAttribute.Left,
+                    1, 0),
+
+                NSLayoutConstraint.Create (
+                    contentItemView,
+                    NSLayoutAttribute.Right,
+                    NSLayoutRelation.Equal,
+                    container,
+                    NSLayoutAttribute.Right,
+                    1, 0),
+
+                NSLayoutConstraint.Create (
+                    contentItemView,
+                    NSLayoutAttribute.Top,
+                    NSLayoutRelation.Equal,
+                    container,
+                    NSLayoutAttribute.Top,
+                    1, 0),
+
+                NSLayoutConstraint.Create (
+                    contentItemView,
+                    NSLayoutAttribute.Bottom,
+                    NSLayoutRelation.Equal,
+                    container,
+                    NSLayoutAttribute.Bottom,
+                    1, 0)
+            };
+        }
+    }
+}
